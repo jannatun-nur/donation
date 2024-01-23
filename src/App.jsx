f43aa37c449830fe2027560bfa5df8c7391758f3
jannatun-nur <jannatunur03@gmail.com>
2024-01-23 15:41:20 +0600

    banner 40%

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Banner from './Banner/Banner'
 import Deposit from './Deposite/Deposit'
 
 import Navbar from './Navbar/Navbar'
@@ -9,13 +10,14 @@ function App() {
  
 
   return (
-    <>
+    <div className='bg-white'>
      <Navbar/>
+     <Banner/>
     <Deposit/>
 
 
 
-    </>
+    </div>
   )
 }
 
--- src/Banner/Banner.jsx
+++ src/Banner/Banner.jsx
@@ -1,11 +1,21 @@
-
-
 const Banner = () => {
-    return (
-        <div>
-            
+  return (
+    <div className="p-6 py-12 dark:bg-violet-400 dark:text-gray-900">
+      <div className="container mx-auto">
+        <div className="flex flex-col lg:flex-row items-center justify-between">
+          <h2 className="text-center text-6xl tracki font-bold">Up to</h2>
+          <div className="space-x-2 text-center py-2 lg:py-0">
+            <span>Plus free shipping! Use code:</span>
+            <span className="font-bold text-lg">MAMBA</span>
+          </div>
+          <div>
+            <img src="" alt="" />
+            <img src="" alt="" />
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
