aa52727d9f37e4922d605cef1f065b237da6c6df
jannatun-nur <jannatunur03@gmail.com>
2024-01-23 21:19:30 +0600

    section 100%

--- src/Deposite/Deposit.jsx
+++ src/Deposite/Deposit.jsx
@@ -25,12 +25,12 @@ const Deposit = () => {
     <div>
        <p className="text-xl lg:text-4xl py-3 text-red-800">Show Your love 💙</p>
       
-          <div className=" columns-1 gap-4 lg:grid grid-cols-2 w-9/12 mx-auto text-white">
-       <section className="bg-rose-500 p-10">
+          <div className=" columns-1 gap-4 lg:grid grid-cols-2 w-9/12 mx-auto text-white ">
+       <section className="bg-rose-500 p-10 mb-2 lg:mb-0">
        <p className="text-xs lg:text-2xl">Donate here</p>
-       <input type="text" id="input-id" placeholder="donate" className="bg-white text-black py-2 rounded-lg" />
+       <input type="text" id="input-id" placeholder="donate" className="bg-white text-black py-1 lg:py-2 rounded-lg" />
 
-       <button onClick={handleClick} id="btn-deposit" className="lg:px-3 lg:py-2 bg-purple-400 rounded-xl text-white p-5">Donate</button>
+       <button onClick={handleClick} id="btn-deposit" className="text-xs lg:text-xl py-1 px-2 lg:px-3 lg:py-2 bg-purple-400 rounded-xl text-white">Donate</button>
      </section>
 
      <section className="bg-sky-500 p-5">
