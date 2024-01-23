a1674e35741ac9cda3f373ef258791a48c0e85c5
jannatun-nur <jannatunur03@gmail.com>
2024-01-23 18:32:51 +0600

    banner 100%

--- src/Banner/Banner.jsx
+++ src/Banner/Banner.jsx
@@ -8,15 +8,15 @@ const Banner = () => {
         <div className="flex flex-col lg:flex-row items-center justify-between">
           
           <div className="space-x-2 text-center py-2 lg:py-0">
-            <p className='text-4xl w-5/12 font-bold'>SHARE YOUR SMILE,
+            <p className='text-xl lg:text-4xl lg:w-5/12 w-11/12 font-bold'>SHARE YOUR SMILE,
              SHOW YOUR INNER KINDNESS.....</p>
              <p className='text-start font-serif text-[#602876]'>One step of yours can be the reason of other's smile.... </p>
             
            
           </div>
           <div>
-            <img className='absolute lg:h-[150px] rounded-lg' src={banner1} alt="" />
-            <img className='relative lg:h-[150px] top-24 right-20 rounded-lg' src={banner2} alt="" />
+            <img className='absolute h-[80px] lg:h-[150px]rounded-lg' src={banner1} alt="" />
+            <img className='relative h-[60px] lg:h-[150px] top-14 lg:top-24 right-20 rounded-lg' src={banner2} alt="" />
           </div>
         </div>
       </div>
