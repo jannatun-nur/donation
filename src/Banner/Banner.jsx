399d44a8a6c4d34bea0314af91d35f88bd2e65ac
jannatun-nur <jannatunur03@gmail.com>
2024-01-23 17:47:59 +0600

    banner 54%

--- src/Banner/Banner.jsx
+++ src/Banner/Banner.jsx
@@ -3,13 +3,14 @@ import banner2 from '../../public/assets/images/banner-2.jpg'
 
 const Banner = () => {
   return (
-    <div className="p-6 py-12 dark:bg-violet-400 dark:text-gray-900">
+    <div className="p-6 py-12 bg-white text-[#602876]">
       <div className="container mx-auto">
         <div className="flex flex-col lg:flex-row items-center justify-between">
           
           <div className="space-x-2 text-center py-2 lg:py-0">
             <p className='text-4xl w-5/12 font-bold'>SHARE YOUR SMILE,
              SHOW YOUR INNER KINDNESS.....</p>
+             <p className='text-start font-serif text-[#602876]'>One step of yours can be the reason of other's smile.... </p>
             
            
           </div>
--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,27 +1,53 @@
-
-import logo from '../../public/assets/images/logo.png'
+import logo from "../../public/assets/images/logo.png";
 const Navbar = () => {
   return (
     <div>
-    <div className="navbar bg-cyan-700 text-white w-full">
-  <div className="flex-1">
-    <p className="px-4 text-xs lg:text-xl">Kindness</p>
-    <div className="dropdown dropdown-end">
-      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-        <div className="w-6 lg:w-10 rounded-full">
-          <img alt="Tailwind CSS Navbar component" src={logo}/>
+      <div className="navbar bg-white text-cyan-500 w-full border border-cyan-700">
+        <div className="flex-1">
+          <p className="px-4 text-xs lg:text-xl">Kindness</p>
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="btn btn-ghost btn-circle avatar"
+            >
+              <div className="w-6 lg:w-10 rounded-full">
+                <img alt="Tailwind CSS Navbar component" src={logo} />
+              </div>
+            </div>
+            <ul
+              tabIndex={0}
+              className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content
+       rounded-box w-52"
+            >
+              <button
+                className="px-4 py-2  bg-white 
+      text-cyan-700 hover:bg-cyan-700 hover:text-white"
+              >
+                Home
+              </button>
+              <button
+                className="px-4 py-2  bg-white 
+      text-cyan-700 hover:bg-cyan-700 hover:text-white"
+              >
+                About
+              </button>
+              <button
+                className="px-4 py-2  bg-white 
+      text-cyan-700 hover:bg-cyan-700 hover:text-white"
+              >
+                Donation
+              </button>
+            </ul>
+          </div>
         </div>
+
+        <input
+          type="text"
+          placeholder="Search"
+          className="w-16 md:w-24 lg:w-28 rounded-lg border border-cyan-700 bg-white text-gray-800"
+        />
       </div>
-      <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-white text-cyan-700 rounded-box w-52">
-        <button>Home</button>
-        <button>About</button>
-        <button>Donation</button>
-      </ul>
-    </div>
-  </div>
- 
- <input type="text" placeholder='Search' className='w-16 md:w-24 lg:w-28 rounded-lg bg-white text-gray-800'/>
-</div>
     </div>
   );
 };
