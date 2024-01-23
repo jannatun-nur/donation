3cdb28e3aeeee26cf9afd65c524777e0b9f70518
jannatun-nur <jannatunur03@gmail.com>
2024-01-23 10:51:10 +0600

    depo 40%

--- src/Deposite/Deposit.jsx
+++ src/Deposite/Deposit.jsx
@@ -3,8 +3,16 @@
 const Deposit = () => {
     return (
         <div>
-         <div>
-            
+         <div className="grid grid-cols-2 gap-4 w-9/12 mx-auto text-white">
+            <section className="bg-sky-500 p-5">
+        <p>Please deposit</p>
+        <input type="text" className="bg-white text-black" />
+            </section>
+            <section className="bg-sky-500 p-5">
+        <p>Total amount</p>
+        <input type="text" className="bg-white text-black" />
+            </section>
+
          </div>
         </div>
     );
