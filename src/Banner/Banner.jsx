91bc1f7d66c711390f9ab99dd15e9f75a0581fc7
jannatun-nur <jannatunur03@gmail.com>
2024-01-23 17:27:36 +0600

    banner 47%

--- src/Banner/Banner.jsx
+++ src/Banner/Banner.jsx
@@ -1,16 +1,21 @@
+import banner1 from '../../public/assets/images/banner-1.jpg'
+import banner2 from '../../public/assets/images/banner-2.jpg'
+
 const Banner = () => {
   return (
     <div className="p-6 py-12 dark:bg-violet-400 dark:text-gray-900">
       <div className="container mx-auto">
         <div className="flex flex-col lg:flex-row items-center justify-between">
-          <h2 className="text-center text-6xl tracki font-bold">Up to</h2>
+          
           <div className="space-x-2 text-center py-2 lg:py-0">
-            <span>Plus free shipping! Use code:</span>
-            <span className="font-bold text-lg">MAMBA</span>
+            <p className='text-4xl w-5/12 font-bold'>SHARE YOUR SMILE,
+             SHOW YOUR INNER KINDNESS.....</p>
+            
+           
           </div>
           <div>
-            <img src="" alt="" />
-            <img src="" alt="" />
+            <img className='absolute lg:h-[150px] rounded-lg' src={banner1} alt="" />
+            <img className='relative lg:h-[150px] top-24 right-20 rounded-lg' src={banner2} alt="" />
           </div>
         </div>
       </div>
