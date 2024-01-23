a1e31db810758ba5f6aa6b9eab655c75d649b3d3
jannatun-nur <jannatunur03@gmail.com>
2024-01-23 11:32:03 +0600

    depo 45%

--- src/Deposite/Deposit.jsx
+++ src/Deposite/Deposit.jsx
@@ -1,12 +1,15 @@
-
-
+import './Deposit'
 const Deposit = () => {
     return (
         <div>
          <div className="grid grid-cols-2 gap-4 w-9/12 mx-auto text-white">
             <section className="bg-sky-500 p-5">
         <p>Please deposit</p>
-        <input type="text" className="bg-white text-black" />
+        <input type="text" id="input-id" className="bg-white text-black" />
+        <button id="deposit"
+        
+        
+        >Deposit</button>
             </section>
             <section className="bg-sky-500 p-5">
         <p>Total amount</p>
@@ -14,7 +17,10 @@ const Deposit = () => {
             </section>
 
          </div>
+         
         </div>
+
+        
     );
 };
 
