1a68b093f5455f42391393e293dfc0557c6dfc16
jannatun-nur <jannatunur03@gmail.com>
2024-01-23 09:33:14 +0600

    navbar 45%

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ function App() {
  
 
   return (
-    <div className='bg-white'>
+    <>
      <Navbar/>
-    </div>
+    </>
   )
 }
 
--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -3,73 +3,33 @@ import logo from '../../public/assets/images/logo.png'
 const Navbar = () => {
   return (
     <div>
-      <header className="p-4 bg-cyan-700 dark:text-gray-100">
-        <div className="container flex justify-between h-16 mx-auto">
-          <ul className='flex gap-2 lg:gap-4 text-xs lg:text-xl pt-4'>
-            <li>Home</li>
-            <li>About</li>
-            <li>Donation</li>
-          </ul>
-          <a
-            rel="noopener noreferrer"
-            href="#"
-            aria-label="Back to homepage"
-            className="flex items-center p-2"
-          >
-           <div className='flex gap-3'>
-        <img className='h-8 w-8 lg:h-10 lg:w-12' src={logo} alt="" />
-        <p className='text-sx lg:text-2xl font-bold  pt-2'>Kindness</p>
-           </div>
-          </a>
-          <div className="flex items-center md:space-x-4">
-            <div className="relative">
-              <span className="absolute inset-y-0 left-0 flex items-center pl-2">
-                <button
-                  type="submit"
-                  title="Search"
-                  className="p-1 focus:outline-none focus:ring"
-                >
-                  <svg
-                    fill="currentColor"
-                    viewBox="0 0 512 512"
-                    className="w-4 h-4 text-black"
-                  >
-                    <path d="M479.6,399.716l-81.084-81.084-62.368-25.767A175.014,175.014,0,0,0,368,192c0-97.047-78.953-176-176-176S16,94.953,16,192,94.953,368,192,368a175.034,175.034,0,0,0,101.619-32.377l25.7,62.2L400.4,478.911a56,56,0,1,0,79.2-79.195ZM48,192c0-79.4,64.6-144,144-144s144,64.6,144,144S271.4,336,192,336,48,271.4,48,192ZM456.971,456.284a24.028,24.028,0,0,1-33.942,0l-76.572-76.572-23.894-57.835L380.4,345.771l76.573,76.572A24.028,24.028,0,0,1,456.971,456.284Z"></path>
-                  </svg>
-                </button>
-              </span>
-              <input
-                type="search"
-                name="Search"
-                placeholder="Search..."
-                className="w-32 py-2 pl-10 text-sm rounded-md sm:w-auto focus:outline-none bg-white  text-gray-800"
-              />
-            </div>
-            <button
-              type="button"
-              className="hidden px-6 py-2  rounded lg:block bg-white text-cyan-700 font-semibold"
-            >
-              Log in
-            </button>
-          </div>
-          <button title="Open menu" type="button" className="p-4 lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              className="w-6 h-6 dark:text-gray-100"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h16M4 18h16"
-              ></path>
-            </svg>
-          </button>
+    <div className="navbar bg-base-100">
+  <div className="flex-1">
+    <a className="btn btn-ghost text-xl">daisyUI</a>
+  </div>
+  <div className="flex-none gap-2">
+    <div className="form-control">
+      <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
+    </div>
+    <div className="dropdown dropdown-end">
+      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+        <div className="w-10 rounded-full">
+          <img alt="Tailwind CSS Navbar component" src={logo}/>
         </div>
-      </header>
+      </div>
+      <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+        <li>
+          <a className="justify-between">
+            Profile
+           
+          </a>
+        </li>
+        <li><a>Settings</a></li>
+        <li><a>Logout</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
     </div>
   );
 };
