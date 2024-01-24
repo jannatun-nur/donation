42e15c1c2880e79620f479b3a324d237fb0b7598
jannatun-nur <jannatunur03@gmail.com>
2024-01-24 09:10:15 +0600

    about 40%

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -3,13 +3,13 @@
 const About = () => {
     return (
         <div>
-           <div className="mx-auto w-5/12 bg-rose-900">
-            <p>KINDNESS</p>
-            <p> Kindness is a blessing for thoose people who are not able to get all facilities.
+           <div className="mx-auto w-full lg:w-5/12 bg-rose-400 p-6 rounded-lg my-4">
+            <p className="text-xs lg:text-2xl text-gray-800">About Us</p>
+            <p className="text-xs lg:text-xl text-gray-800"> Kindness is a blessing for thoose people who are not able to get all facilities.
                 Kindness brings for them a litle bit helps.It was created since 2003. Our main target is 
                 helps other in easy and effortable way. Keep with us and makes our jouenry easy. If you also want
                 to help other, let us know:</p>
-                <ul>
+                <ul className="pt-3 text-xs lg:text-xl text-gray-800">
                     <li>☎️  +56975124</li>
                     <li>📧  kindnessofficial@gmail.com</li>
                     <li>🌐  www.kindness.gloval.com</li>
